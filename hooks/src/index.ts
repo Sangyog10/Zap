4d49c656e1baf2263090f5969a5656109e0cd5a8
Sangyog Puri <sangyogpuri@Sangyogs-MacBook-Air.local>
2024-10-23 16:59:16 +0545

    added kafka as queue:

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -4,19 +4,23 @@ import { PrismaClient } from "@prisma/client";
 const app = express();
 const client = new PrismaClient();
 
+//webhook implemtation(i.e whenever we receive req from other services)
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
   const body = req.body;
 
+  //transaction will make sure that both works get completed
   await client.$transaction(async (tx) => {
-    const run = await client.zapRun.create({
+    //storing in db
+    const run = await tx.zapRun.create({
       data: {
         zapId: zapId,
         metadata: body,
       },
     });
-    await client.zapRunOutbox.create({
+    // storing in  queue
+    await tx.zapRunOutbox.create({
       data: {
         zapRunId: run.id,
       },
@@ -29,3 +33,6 @@ app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
 });
 
 app.listen(3000);
+
+//webhook is a hook that lets differnt platform send request to our system.
+//eg, whenever someone comments in github, webhook will notify zapier about it if they are connected
---
+++ processor/src/index.ts
@@ -0,0 +1,44 @@
+//the work of this process is to pull things from outbox(zapRunOutbox) and push it to the kafka queue
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+
+const client = new PrismaClient();
+
+const TOPIC_NAME = "zap-events"; //topic created for kafka via terminal
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  while (1) {
+    //retriving value from outbox
+    const pendingRows = await client.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    //sending to kafka queue
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((r) => {
+        return { value: r.zapRunId };
+      }),
+    });
+
+    //deleting after sending from queue
+    await client.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((r) => r.id),
+        },
+      },
+    });
+  }
+}
+
+main();
